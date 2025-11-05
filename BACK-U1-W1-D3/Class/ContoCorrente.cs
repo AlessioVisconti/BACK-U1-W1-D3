b059cf69a91b6dc392be64ccb64dce88f1abfe82
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-05 16:32:12 +0100

    Aggiungere i file di progetto.

---
+++ BACK-U1-W1-D3/Class/ContoCorrente.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACK_U1_W1_D3.Class
+{
+    public class ContoCorrente
+    {
+        //il conto ha: Nome, cognome, numero carta, secret number, iban, scadanza, saldo
+        //metodo per caricare il conto
+        //metodo per prelevare
+
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string NumDocumento { get; set; }
+        public string NumeroConto { get; set; }
+        public int NumeroCarta { get; set; }
+        public string Iban { get; set; }
+        private string Pin { get; set; }
+        private int CVV { get; set; }
+        
+        public string DataFine { get; set; }
+        public int Saldo { get; set; }
+
+  
+
+        //public ContoCorrente(string nome, string cognome, string numDocumento, string numeroConto, int numeroCarta, string iban, string pin, int cvv, string dataFine="08/24", int saldo = 0)
+        //{
+        //    this.Nome = nome;
+        //    this.Cognome = cognome;
+        //    this.NumDocumento = numDocumento;
+        //    this.NumeroConto = numeroConto;
+        //    this.NumeroCarta = numeroCarta;
+        //    this.Iban = iban;
+        //    this.Pin = pin;
+        //    this.CVV = cvv;
+        //    this.DataFine = dataFine;
+        //    this.Saldo = saldo;
+            
+        //}
+        public static string[] documenti = { "AX1111", "AX2222", "AX3333" };
+        public void ApriConto() {
+            Console.WriteLine("Stiamo aprendo il conto");
+            Console.WriteLine("Inserisci il tuo numero di documento: ");
+            string numDocumento = Console.ReadLine();
+            foreach(string doc in documenti)
+            {
+                if(doc== numDocumento)
+                {
+                    Console.WriteLine("Esiste già un conto");
+                    return;
+                }
+            }
+            
+            Console.WriteLine("Inserisci il tuo nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo cognome: ");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci il Pin: ");
+            string pin = Console.ReadLine();
+            Random rnd = new Random();
+            string numeroConto = "";
+            string numeroCartaProvvisorio = "";
+            string numeroCartaProvvisorioCVV = "";
+            string iban = "IT00";
+            
+            for (int i = 0; i < 16; i++)
+            {
+                int rndNumero = rnd.Next(0, 10);
+                numeroConto += rndNumero.ToString();
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+            int rndNumero = rnd.Next(0, 10);
+                numeroCartaProvvisorio += rndNumero.ToString();
+            
+            }
+            int numeroCarta = int.Parse(numeroCartaProvvisorio);
+           
+            for (int i = 0; i < 16; i++)
+            {
+                int rndNumero = rnd.Next(0, 10);
+                iban += rndNumero.ToString();
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                int rndNumero = rnd.Next(0, 10);
+                numeroCartaProvvisorioCVV += rndNumero.ToString();
+
+            }
+            int cvv = int.Parse(numeroCartaProvvisorioCVV);
+            Console.WriteLine("E' richiesto un deposito di 1000€ per aprire il conto vuole procedere?");
+            Console.WriteLine("1: si");
+            Console.WriteLine("2: no");
+            string scelta = Console.ReadLine();
+            switch (scelta)
+            {
+                case "1":
+                   Saldo= 1000;
+                    break;
+                case "2":
+                    break;
+            }
+          
+            Console.WriteLine("Il tuo conto è stato aperto con successo, ecco un riepilogo dei tuoi dati");
+            Console.WriteLine($"Numero conto: {numeroConto}");
+            Console.WriteLine($"Iban: {iban}");
+            Console.WriteLine($"Saldo iniziale: {Saldo}");
+            
+
+
+           
+            
+        }
+        public void Prelievo()
+        {
+            Console.WriteLine("Quanto vuoi prelevare?");
+            string prelievo = Console.ReadLine();
+            int prelievoParsato = int.Parse(prelievo);
+            if (Saldo < prelievoParsato)
+            {
+                Console.WriteLine("Credito insufficiente");
+            }
+            else
+            {
+                int saldo = -prelievoParsato;
+                Console.WriteLine($"il nuovo saldo è: {saldo}");
+            }
+        }
+        public void Deposita()
+        {
+            Console.WriteLine("Quanto vuoi depositare?");
+            string deposito = Console.ReadLine();
+            int depositoParsato = int.Parse(deposito);
+            int saldo=+ depositoParsato;
+            Console.WriteLine($"il nuovo saldo è: {saldo}");
+        }
+       
+    }
+}
---
+++ BACK-U1-W1-D3/Program.cs
@@ -0,0 +1,65 @@
+﻿/*1) Scrivere una classe ContoCorrente che permetta di archiviare diverse proprietà del conto, 
+ * più il relativo saldo. In più permetta di effettuare le seguenti azioni:
+
+-Aprire il conto
+
+- Fare un versamento
+
+- Fare un prelievo
+
+N.B.: L'apertura del conto può essere effettuata solo una volta e 
+contestualmente deve necessariame
+nte consentire un versamento almeno 1000 euro.*/
+
+using BACK_U1_W1_D3.Class;
+using System.Security.Cryptography.X509Certificates;
+
+bool booleano = true;
+while (booleano) { 
+Console.WriteLine("scegli l'operazione da effettuare: ");
+Console.WriteLine("1: aprire il Conto");
+
+Console.WriteLine("2: esci");
+string scelta = Console.ReadLine();
+    bool stato = false;
+  ContoCorrente user= new ContoCorrente();
+    switch(scelta){
+        case "1":
+            user.ApriConto();
+            stato = true;
+           
+            if (stato && user.Saldo>=1000)
+            {
+                Console.WriteLine("Decidi la prossima operazione: ");
+                Console.WriteLine("1: versare");
+                Console.WriteLine("2: prelevare");
+                Console.WriteLine("3: esci");
+                scelta = Console.ReadLine();
+                switch (scelta)
+                {
+                    case "1":
+                        user.Deposita();
+                        break;
+                    case "2":
+                        user.Prelievo();
+                        break;
+                    case "3":
+                        booleano = false;
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Non hai deposito i 1000€ richiesti");
+            }
+                break;
+        case "2":
+            booleano = false;
+            break;
+    }
+    
+    
+}
+//2) Scrivere un algoritmo che prenda in input un valore di x nomi (decida il candidato la dimensione dell'array).
+//Dopo aver caricato l'array, specificare un nome da ricercare e stampare se il nome è presente o
+//meno nell'array caricato precedentemente.
