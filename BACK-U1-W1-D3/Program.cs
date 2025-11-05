1ead5fa5e9bd033d7e182f76b9601c86b941d72c
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-05 16:52:04 +0100

    end ex3

--- BACK-U1-W1-D3/Program.cs
+++ BACK-U1-W1-D3/Program.cs
@@ -15,51 +15,51 @@ using BACK_U1_W1_D3.Class;
 using System.Security.Cryptography.X509Certificates;
 
 bool booleano = true;
-while (booleano) { 
-Console.WriteLine("scegli l'operazione da effettuare: ");
-Console.WriteLine("1: aprire il Conto");
+//while (booleano) { 
+//Console.WriteLine("scegli l'operazione da effettuare: ");
+//Console.WriteLine("1: aprire il Conto");
 
-Console.WriteLine("2: esci");
-string scelta = Console.ReadLine();
-    bool stato = false;
-  ContoCorrente user= new ContoCorrente();
-    switch(scelta){
-        case "1":
-            user.ApriConto();
-            stato = true;
+//Console.WriteLine("2: esci");
+//string scelta = Console.ReadLine();
+//    bool stato = false;
+//  ContoCorrente user= new ContoCorrente();
+//    switch(scelta){
+//        case "1":
+//            user.ApriConto();
+//            stato = true;
            
-            if (stato && user.Saldo>=1000)
-            {
-                Console.WriteLine("Decidi la prossima operazione: ");
-                Console.WriteLine("1: versare");
-                Console.WriteLine("2: prelevare");
-                Console.WriteLine("3: esci");
-                scelta = Console.ReadLine();
-                switch (scelta)
-                {
-                    case "1":
-                        user.Deposita();
-                        break;
-                    case "2":
-                        user.Prelievo();
-                        break;
-                    case "3":
-                        booleano = false;
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Non hai deposito i 1000€ richiesti");
-            }
-                break;
-        case "2":
-            booleano = false;
-            break;
-    }
+//            if (stato && user.Saldo>=1000)
+//            {
+//                Console.WriteLine("Decidi la prossima operazione: ");
+//                Console.WriteLine("1: versare");
+//                Console.WriteLine("2: prelevare");
+//                Console.WriteLine("3: esci");
+//                scelta = Console.ReadLine();
+//                switch (scelta)
+//                {
+//                    case "1":
+//                        user.Deposita();
+//                        break;
+//                    case "2":
+//                        user.Prelievo();
+//                        break;
+//                    case "3":
+//                        booleano = false;
+//                        break;
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Non hai deposito i 1000€ richiesti");
+//            }
+//                break;
+//        case "2":
+//            booleano = false;
+//            break;
+//    }
     
     
-}
+//}
 //2) Scrivere un algoritmo che prenda in input un valore di x nomi (decida il candidato la dimensione dell'array).
 //Dopo aver caricato l'array, specificare un nome da ricercare e stampare se il nome è presente o
 //meno nell'array caricato precedentemente.
@@ -76,3 +76,23 @@ for (int i = 0; i < lunghezzaArray; i++)
     Console.WriteLine($"{nomi[i]}");
 
 }
+/*3) Scrivere un algoritmo che prenda in input:
+
+	a) La dimensione di un array
+
+	b) In base alla dimensione dell'array, x numeri interi
+
+e restituisca:
+
+	a) La somma di tutti i numeri inseriti all'interno dell'array
+
+	b) La media aritmetica di tutti i numeri inseriti all'interno dell'array*/
+Console.WriteLine("scrivi la lunghezza dell'arrayX");
+int lunghezzaArrayX = int.Parse(Console.ReadLine());
+int[] numeri = { 1, 2, 3, 4, 5, 6 };
+int somma = 0;
+for(int i=0; i < lunghezzaArrayX; i++)
+{
+   somma= somma + numeri[i];
+}
+Console.WriteLine(somma);
